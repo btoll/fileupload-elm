c4634ce83b0793085fa287bee8fa2d31aafd7a87
Benjamin Toll <benjam72@yahoo.com>
2017-11-05 17:34:46 -0500

    First successful request of base64 encoded image sent to server

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,19 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (action, for, id, title, type_, value)
-import Html.Events exposing (on)
+import Html.Attributes exposing (action, enctype, for, id, title, type_, value)
+import Html.Events exposing (on, onSubmit)
+import Http
 import Json.Decode as Decode
+import Json.Encode as Encode
 import FileUpload exposing (FileData, fileSelected, fileContentRead)
 
 
 type Msg
     = FileSelected
     | FileRead FileData
+    | SubmitFile String
+    | PostFile ( Result Http.Error String )
 
 type alias File =
     { contents : String
@@ -20,6 +24,16 @@ type alias Model =
     { id : String
     , file : Maybe File
     }
+
+-- https://github.com/rtfeldman/elm-spa-example/blob/master/src/Util.elm#L8
+(=>) : a -> b -> ( a, b )
+(=>) =
+    (,)
+
+{-| infixl 0 means the (=>) operator has the same precedence as (<|) and (|>),
+meaning you can use it at the end of a pipeline and have the precedence work out.
+-}
+infixl 0 =>
 
 main : Program Never Model Msg
 main =
@@ -32,7 +46,7 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { id = "foo"
+    ( { id = "Banner"
       , file = Nothing
       }
     , Cmd.none
@@ -53,14 +67,49 @@ update msg model =
             in
                 ( { model | file = Just newFile } , Cmd.none )
 
+        SubmitFile url ->
+            ( model, makeRequest url model.file )
+
+        -- TODO
+        PostFile ( Ok _ ) ->
+            ( model, Cmd.none )
+
+        -- TODO
+        PostFile ( Err _ ) ->
+            ( model, Cmd.none )
+
+makeRequest : String -> Maybe File -> Cmd Msg
+makeRequest url file =
+    let
+        objectfile =
+            Maybe.withDefault { contents = "", filename = "" } file
+
+        json =
+            Encode.object
+                [ "filename" => Encode.string objectfile.filename
+                , "contents" => Encode.string objectfile.contents
+                ]
+
+        body =
+            Encode.list [ json ]
+                |> Http.jsonBody
+
+        request =
+            Http.post url body Decode.string
+
+
+    in
+        Http.send PostFile request
+
 view : Model -> Html Msg
 view model =
     form
-    [ action "http://localhost:8000/nmg" ]
+--    [ action "http://localhost:8080/nmg/image/team/5", onSubmit SubmitFile ]
+    [ onSubmit ( SubmitFile "http://localhost:8080/nmg/image/team/5" ) ]
     ( List.concat [
         [ div [] [ select [] ( List.map optionsList [ "-- Choose Sport --", "Baseball", "Basketball", "Golf", "Soccer", "Tennis" ] ) ] ]
         , ( List.map fileUploadField [ "Banner", "Logo", "Icon" ] )
-        , [ div [] [ input [ type_ "submit"] [ text "Upload" ] ] ]
+        , [ div [] [ input [ type_ "submit" ] [ text "Upload" ] ] ]
     ] )
 
 fileUploadField : String -> Html Msg
